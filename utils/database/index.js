3ff8e236b0e116de444084e84c1dcc7d2b45a155
Shoaib Merchant <shoaib@actonate.com>
2017-11-19 20:00:19 +0530

    added joins support for hasOne, hasMany and belongsTo relations

--- utils/database/index.js
+++ utils/database/index.js
@@ -1,6 +1,7 @@
 import Database from './database';
 import DatabaseMutations from './mutations';
 import DatabaseQueries from './queries';
+import Types from '../../lib/types';
 
 class DatabaseUtility {
 
@@ -19,6 +20,125 @@ class DatabaseUtility {
 
 	queries(type, inputType, model) {
 		return DatabaseQueries.generate(type, inputType, model);
+	}
+
+	relations(model) {
+		let modelSchema = model.schema;
+		let modelRelations = modelSchema.relations;
+		let modelName = model.schema.name;
+		let modelKey = model.key;
+
+		let modelFields = modelSchema.fields;
+
+		// Children or hasMany
+		if (modelRelations.children && Array.isArray(modelRelations.children)) {
+			modelFields['_children'] = {
+				type: {
+					fields: () => this._generateChildrenRelations(modelKey, modelRelations.children)
+				},
+				resolve: (obj) => obj
+			};
+		}
+
+		// Child or hasOne
+		if (modelRelations.child && Array.isArray(modelRelations.child)) {
+			modelFields['_child'] = {
+				type: {
+					fields: () => this._generateChildRelations(modelKey, modelRelations.child)
+				},
+				resolve: (obj) => obj
+			};
+		}
+
+		// Parent or belongs
+		if (modelRelations.parent && Array.isArray(modelRelations.parent)) {
+			modelFields['_parent'] = {
+				type: {
+					fields: () => this._generateParentRelations(modelKey, modelRelations.parent)
+				},
+				resolve: (obj) => obj
+			};
+		}
+
+		return model;
+	}
+
+	_generateChildrenRelations(modelKey, relations) {
+		let childrenRelations = {};
+
+		relations.forEach((relation) => {
+			let resolverName = [modelKey, 'children', relation.key].join('.');
+
+			childrenRelations[relation.key] = this.resolvers['joinMany'](
+				resolverName,
+				Types.get(relation.type), Types.model(relation.model),
+				{
+					args: {
+						field: {
+							type: Types.String,
+							defaultValue: relation.field
+						},
+						joinBy: {
+							type: Types.String,
+							defaultValue: relation.joinBy || '_id'
+						}
+					}
+				});
+		});
+
+		return childrenRelations;
+	}
+
+	_generateChildRelations(modelKey, relations) {
+		let childRelations = {};
+
+		relations.forEach((relation) => {
+			let resolverName = [modelKey, 'child', relation.key].join('.');
+
+			childRelations[relation.key] = this.resolvers['joinOne'](
+				resolverName,
+				Types.get(relation.type), Types.model(relation.model),
+				{
+					args: {
+						field: {
+							type: Types.String,
+							defaultValue: relation.field
+						},
+						joinBy: {
+							type: Types.String,
+							defaultValue: relation.joinBy || '_id'
+						}
+					}
+				});
+		});
+
+		return childRelations;
+	}
+
+	_generateParentRelations(modelKey, relations) {
+		let parentRelations = {};
+
+		relations.forEach((relation) => {
+			let resolverName = [modelKey, 'parent', relation.key].join('.');
+
+			parentRelations[relation.key] = this.resolvers['joinOne'](
+				resolverName,
+				Types.get(relation.type), Types.model(relation.model),
+				{
+					args: {
+						field: {
+							type: Types.String,
+							defaultValue: relation.field || '_id'
+						},
+						joinBy: {
+							type: Types.String,
+							defaultValue: relation.joinBy 
+						}
+					}
+				});
+		});
+
+		return parentRelations;
 	}
 }
 
