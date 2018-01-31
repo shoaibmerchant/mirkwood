73dbda9abdf6f76e4ac82b9e0fea3f5cbaf8ae81
Raghu Mudaliar <raghu.mudaliar@actonate.com>
2018-01-31 20:02:35 +0530

    dis_max and all search

--- utils/elasticsearch/index.js
+++ utils/elasticsearch/index.js
@@ -3,6 +3,7 @@ import Types from '../../lib/types';
 import Resolver from '../../lib/resolver';
 import Elasticsearch from './elasticsearch';
 import {GraphQLObjectType} from 'graphql';
+import queries from '../database/queries';
 
 class ElasticsearchUtility {
   constructor({config}) {
@@ -11,60 +12,21 @@ class ElasticsearchUtility {
       index: this
         .indexResolver
         .bind(this),
-      query: this
+      match: this
+        .matchResolver
+        .bind(this),
+      term: this
+        .termResolver
+        .bind(this),
+      range: this
+        .rangeResolver
+        .bind(this),
+      search: this
         .searchResolver
         .bind(this)
     };
   }
-
-  indexResolver(resolverName, type, model, inputSchema) {
-    const client = this.client;
-    let args = inputSchema.args;
-    let schema = model.schema;
-    let modelName = schema.name;
-    args = {
-      ...args
-    };
-
-    let argsObjects = Types.generateArgs(args, inputSchema.name);
-    return {
-      type: Types.Boolean,
-      args: argsObjects,
-      resolve: new Resolver(resolverName, (_, args, ctx) => {
-        return client.index(modelName, args._id, args.input);
-      })
-    };
-  }
-
-  generateMatchType(model) {
-		let schema = model.schema;
-		let modelName = schema.name;
-
-		let matchTypeName = [modelName, 'Match'].join('_');
-
-		// fetch if exists
-		if (Types.get(matchTypeName)) {
-			return Types.get(matchTypeName);
-		}
-
-		return Types.generateInputType({
-		  name: matchTypeName,
-		  fields: schema.fields
-		}, ['defaultValue']); // sp that defaultValue is filtered out
-  }
-  
-  searchResolver(resolverName, type, model, inputSchema) {
-    const client = this.client;
-    let args = inputSchema.args;
-    let schema = model.schema;
-    let modelName = schema.name;
-    args = {
-      ...args
-    }
-    let argsObjects = Types.generateArgs(args, inputSchema.name);
-
-    const resolvedTypeName = [modelName, inputSchema.name].join('');
-
+  queryReturnTypeGenerator(resolvedTypeName, type) {
     const resolvedType = Types.generateType({
       name: [resolvedTypeName, 'Elastic_Search'].join(''),
       fields: {
@@ -92,22 +54,214 @@ class ElasticsearchUtility {
         }
       }
     });
+    return resolvedType;
+  }
 
+  indexResolver(resolverName, type, model, inputSchema) {
+    const client = this.client;
+    let args = inputSchema.args;
+    let schema = model.schema;
+    let modelName = schema.name;
+    args = {
+      ...args
+    };
+
+    let argsObjects = Types.generateArgs(args, inputSchema.name);
+    return {
+      type: Types.Boolean,
+      args: argsObjects,
+      resolve: new Resolver(resolverName, (_, args, ctx) => {
+        return client.index(modelName, args._id, args.input);
+      })
+    };
+  }
+
+  generateMatchType(model) {
+    let schema = model.schema;
+    let modelName = schema.name;
+
+    let matchTypeName = [modelName, 'Match'].join('_');
+
+    // fetch if exists
+    if (Types.get(matchTypeName)) {
+      return Types.get(matchTypeName);
+    }
+
+    return Types.generateInputType({
+      name: matchTypeName,
+      fields: schema.fields
+    }, ['defaultValue']); // sp that defaultValue is filtered out
+  }
+
+  
+
+  matchResolver(resolverName, type, model, inputSchema) {
+    const client = this.client;
+    let args = inputSchema.args;
+    let schema = model.schema;
+    let modelName = schema.name;
+    args = {
+      ...args
+    }
+    let argsObjects = Types.generateArgs(args, inputSchema.name);
+    const resolvedTypeName = [modelName, inputSchema.name].join('');
+    const resolvedType = this.queryReturnTypeGenerator(resolvedTypeName, type);
     return {
       type: resolvedType,
       args: argsObjects,
       resolve: new Resolver(resolverName, (_, args, ctx) => {
+        let newfield = [];
+        args
+          .fields
+          .map(field => {
+            let field_data = [field.name, field.boost].join('^')
+            newfield.push(field_data);
+          });
         let match = {
           query: {
-            match: args.match
+            multi_match: {
+              query: args.query,
+              fields: newfield,
+              fuzziness: args.fuzziness || 2
+            }
           }
         }
-        console.log("match",match);
         return client.search(modelName, match);
       })
     };
   }
 
+  termResolver(resolverName, type, model, inputSchema) {
+    const client = this.client;
+    let args = inputSchema.args;
+    let schema = model.schema;
+    let modelName = schema.name;
+    args = {
+      ...args
+    }
+    let argsObjects = Types.generateArgs(args, inputSchema.name);
+    const resolvedTypeName = [modelName, inputSchema.name].join('');
+    const resolvedType = this.queryReturnTypeGenerator(resolvedTypeName, type);
+    return {
+      type: resolvedType,
+      args: argsObjects,
+      resolve: new Resolver(resolverName, (_, args, ctx) => {
+        let term = {};
+        term[args.field] = args.value;
+        let match = {
+          query: {
+            terms: term
+          }
+        }
+        return client.search(modelName, match);
+      })
+    };
+  }
+
+  rangeResolver(resolverName, type, model, inputSchema) {
+    const client = this.client;
+    let args = inputSchema.args;
+    let schema = model.schema;
+    let modelName = schema.name;
+    args = {
+      ...args
+    }
+    let argsObjects = Types.generateArgs(args, inputSchema.name);
+    const resolvedTypeName = [modelName, inputSchema.name].join('');
+    const resolvedType = this.queryReturnTypeGenerator(resolvedTypeName, type);
+    return {
+      type: resolvedType,
+      args: argsObjects,
+      resolve: new Resolver(resolverName, (_, args, ctx) => {
+        let range = {};
+        range[args.field] = {
+          gte: args.greaterThanEqual,
+          lte: args.lessThanEqual,
+          boost: args.boost
+        }
+        let match = {
+          query: {
+            range: range
+          }
+        }
+        return client.search(modelName, match);
+      })
+    };
+  }
+
+  searchResolver(resolverName, type, model, inputSchema) {
+    const client = this.client;
+    let args = inputSchema.args;
+    let schema = model.schema;
+    let modelName = schema.name;
+    args = {
+      ...args
+    }
+    let argsObjects = Types.generateArgs(args, inputSchema.name);
+    const resolvedTypeName = [modelName, inputSchema.name].join('');
+    const resolvedType = this.queryReturnTypeGenerator(resolvedTypeName, type);
+    return {
+      type: resolvedType,
+      args: argsObjects,
+      resolve: new Resolver(resolverName, (_, args, ctx) => {
+        let query = [];
+        let newfield = [];
+        if (args.match) {
+          args
+            .match
+            .fields
+            .map(field => {
+              let field_data = [field.name, field.boost].join('^')
+              newfield.push(field_data);
+            });
+          let matchData = {
+            multi_match: {
+              query: args.match.query,
+              fields: newfield,
+              fuzziness: args.match.fuzziness || 2
+            }
+          };
+          query.push(matchData);
+        }
+        if (args.range) {
+          args
+            .range
+            .map(rangeData => {
+              let data = {};
+              data[rangeData.field] = {
+                gte: rangeData.greaterThanEqual,
+                lte: rangeData.lessThanEqual,
+                boost: rangeData.boost
+              };
+              let ranges = {}
+              ranges["range"] = data;
+              query.push(ranges);
+            });
+        }
+        if (args.term) {
+          args
+            .term
+            .map(termData => {
+              let term = {};
+              let terms = {};
+              term[termData.field] = termData.value;
+              terms["terms"] = term;
+              query.push(terms);
+            })
+        }
+        let match = {
+          query: {
+            dis_max: {
+              tie_breaker: args.tie_breaker || 0,
+              boost: args.boost,
+              queries: query
+            }
+          }
+        }
+        return client.search(modelName, match);
+      })
+    };
+  }
   mutations(type, inputType, model) {
     let schema = model.schema;
     let modelName = schema.name;
@@ -131,13 +285,103 @@ class ElasticsearchUtility {
   queries(type, inputType, model) {
     let schema = model.schema;
     let modelName = schema.name
+
+    const elasticQueryMatchFieldType = Types.generateInputType({
+      name: [modelName, 'Elastic_Query_Field'].join(''),
+      fields: {
+        name: {
+          type: Types.String
+        },
+        boost: {
+          type: Types.Float
+        }
+      }
+    });
+
+    const matchFieldType = Types.generateInputType({
+      name: [modelName, 'Elastic_Query_Match_Field'].join(''),
+      fields: {
+        query: {
+          type: Types.String
+        },
+        fields: {
+          type: Types.List(elasticQueryMatchFieldType)
+        },
+        fuzziness: {
+          type: Types.Int
+        }
+      }
+    });
+    const termFieldType = Types.generateInputType({
+      name: [modelName, 'Elastic_Query_Term_Field'].join(''),
+      fields: {
+        field: {
+          type: Types.String
+        },
+        value: {
+          type: Types.List(Types.String)
+        }
+      }
+    });
+    const rangeFieldType = Types.generateInputType({
+      name: [modelName, 'Elastic_Query_Range_Field'].join(''),
+      fields: {
+        field: {
+          type: Types.String
+        },
+        greaterThanEqual: {
+          type: Types.Float
+        },
+        lessThanEqual: {
+          type: Types.Float
+        },
+        boost: {
+          type: Types.Float
+        }
+      }
+    });
+
     return new GraphQLObjectType({
       name: [modelName, 'Elastic_Query'].join(''),
       fields: {
+
+        match: this.matchResolver('elasticsearch.match', type, model, {
+          args: {
+            input: {
+              type: matchFieldType
+            }
+          }
+        }),
+        term: this.termResolver('elasticsearch.term', type, model, {
+          args: {
+            input: {
+              type: termFieldType
+            }
+          }
+        }),
+        range: this.rangeResolver('elasticsearch.range', type, model, {
+          args: {
+            input: {
+              type: rangeFieldType
+            }
+          }
+        }),
         search: this.searchResolver('elasticsearch.search', type, model, {
           args: {
+            range: {
+              type: Types.List(rangeFieldType)
+            },
+            term: {
+              type: Types.List(termFieldType)
+            },
             match: {
-              type: this.generateMatchType(model)
+              type: matchFieldType
+            },
+            boost: {
+              type: Types.Float
+            },
+            tie_breaker: {
+              type: Types.Float
             }
           }
         })
